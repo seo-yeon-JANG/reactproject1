11b2d3d42ad50bc9a0bf7a6fb848da9cd610c339
seoyeon <tjsus247@naver.com>
2022-10-27 15:52:58 +0900

    filter 기능 추가

--- src/componetns/Expense/ExpenseItem.js
+++ src/componetns/Expense/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card.js';
@@ -7,22 +7,15 @@ import './ExpenseItem.css';
 
 const ExpenseItem = (props) =>{
 
-   const [title, SetTitle] =  useState(props.title);
-
-    const clickHandler = () =>{
-        SetTitle('updated!');
-
-    };
 
 
     return (
     <Card className='expense-item'>
         <ExpenseDate date={props.date} />
         <div className='expense-item__description'>
-            <h2>{title}</h2>
+            <h2>{props.title}</h2>
             <div className='expense-item__price'>{props.amount}</div>
         </div>
-        <button onClick={clickHandler}>Change Title</button>
     </Card>
     );
 }
--- src/componetns/Expense/Expenses.js
+++ src/componetns/Expense/Expenses.js
@@ -5,23 +5,43 @@ import './Expenses.css'
 import ExpensesFilter from "./ExpensesFilter.js";
 
 const  Expenses = (props) => {
+    // console.log(props.item)
     const [filteredYear, setFilteredYear] =  useState('2020');
 
     const filterChangeHandler = selectedYear =>{
         setFilteredYear(selectedYear);
+    };
+
+    const filteredExpenses = props.item.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    }); //true/false 값을 반환, 선택한 날짜와 목록에 있는 날짜와 같은 것을 함수에 저장함
+
+    let expensesContent = <p>No expenses found.</p>;
+
+    if (filteredExpenses.length > 0 ) {
+        expensesContent = filteredExpenses.map((item) => ( 
+        <ExpenseItem 
+            key={item.id}  
+            title={item.title} 
+            amount={item.amount} 
+            date={item.date} 
+            />//동적
+        ))
     }
-    
 
        return(
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                <ExpenseItem title={props.item[0].title} amount={props.item[0].amount} date={props.item[0].date}/>
+                
+                {expensesContent}
+          
+                {/* <ExpenseItem title={props.item[0].title} amount={props.item[0].amount} date={props.item[0].date}/>
                 <ExpenseItem title={props.item[1].title} amount={props.item[1].amount} date={props.item[1].date}/>
                 <ExpenseItem title={props.item[2].title} amount={props.item[2].amount} date={props.item[2].date}/>
-                <ExpenseItem title={props.item[3].title} amount={props.item[3].amount} date={props.item[3].date}/>
+                <ExpenseItem title={props.item[3].title} amount={props.item[3].amount} date={props.item[3].date}/> 정적*/}
             </Card>
         </div>
        ) 
-}
+} //목록을 매핑할 때 key값이 중요
 export default Expenses;
